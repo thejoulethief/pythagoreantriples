3b3ac83e59ce3246498a6d0a5f6cb495067ba317
thejoulethief <anupamsanthosh@gmail.com>
2019-08-25 23:00:26 +0530

    fermat's last theorem

--- pythagoreanTriples.js
+++ pythagoreanTriples.js
@@ -1,15 +1,19 @@
-// findPythagoreanTriples(positive integer to start from, max integer to consider, whether only primitive triples should be considered);
-findPythagoreanTriples(1, 100, true);
-function findPythagoreanTriples(start, chunk_size, checkForPrimitive) {
+// findPythagoreanTriples(positive integer to start from, max integer to consider, Boolean:whether only primitive triples should be considered, exponent for equation (possible values:2,3));
+findPythagoreanTriples(1, 10000, true, 3);
+function findPythagoreanTriples(start, chunk_size, checkForPrimitive, exponent) {
     if (start <= 0 || chunk_size < start) throw "Invalid range!";
-    console.log("Finding solutions for a^2+b^2 = c^2...")
+    if (exponent == 3) console.log("Finding integer solutions for a^3 + b^3 = c^3...bit pointless after they proved Fermat's Last Theorem, isn't it?")
+    else console.log("Finding integer solutions for a^2 + b^2 = c^2...")
     notCompleted = true;
+    let count = 0;
     while (notCompleted) {
         let list = [];
         for (let a = start; a <= chunk_size; a++) {
             for (let b = start; b <= chunk_size; b++) {
-                let c_squared = Math.pow(a, 2) + Math.pow(b, 2);
-                let c = Math.sqrt(c_squared);
+                let c_squared = Math.pow(a, exponent) + Math.pow(b, exponent);
+                let c;
+                if (exponent == 3) { c = Math.cbrt(c_squared); }
+                else { c = Math.sqrt(c_squared); }
                 if (c % 1 == 0) {
                     if (list.includes(c)) continue;
                     list.push(c);
@@ -21,9 +25,13 @@ function findPythagoreanTriples(start, chunk_size, checkForPrimitive) {
                                 continue;
                             }
                         }
-                        if (!notPrimitive) console.log(`Found a primitive pythagorean triple, ${a}^2 + ${b}^2 = ${c}^2`)
+                        if (!notPrimitive) {
+                            count++;
+                            console.log(`Found a primitive pythagorean triple, ${a}^2 + ${b}^2 = ${c}^2`)
+                        }
                     }
                     else {
+                        count++;
                         console.log(`Found a pythagorean triple, ${a}^2 + ${b}^2 = ${c}^2`);
                     }
                 }
@@ -32,4 +40,5 @@ function findPythagoreanTriples(start, chunk_size, checkForPrimitive) {
         }
         notCompleted = false;
     }
+    console.log(`Finished after finding ${count} triples.`)
 }
